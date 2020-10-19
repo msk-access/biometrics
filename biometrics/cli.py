f7b6c5467b989e5a9992841317c2a16cd64193ec
murphycj2 <murphy.charlesj@icloud.com>
2020-10-19 16:30:59 -0400

    very basic cli finished

--- biometrics/cli.py
+++ biometrics/cli.py
@@ -1,6 +1,7 @@
 """Console script for biometrics."""
-import sys
 import argparse
+
+from .utils import exit_error
 
 
 def add_common_args(parser):
@@ -24,8 +25,31 @@ def add_common_args(parser):
         '-sn', '--sample-name', help='''Space-delimited list of sample names.
         If not specified, sample name is automatically figured out from the
         BAM file. Must be in the same order as --sample-bam.''')
+    parser.add_argument(
+        '-db', '--database',
+        default=['/juno/work/access/production/data/bams'],
+        help='''Directory to store the intermediate files after
+        running the extraction step.''')
+    parser.add_argument(
+        '--bam-basedir', default='/juno/work/access/production/data/bams',
+        help='''Base directory where BAM files are located if you specified
+        title files.''')
 
     return parser
+
+
+def add_outdir(parser):
+    parser.add_argument(
+        '-o', '--outdir', default='.',
+        help='''Output directory for results.''')
+
+    return parser
+
+
+def check_args(args):
+
+    if not args.titlefile and not args.sample_bam:
+        exit_error('You must specify either --titlefile or --sample-bam')
 
 
 def get_args():
@@ -40,29 +64,35 @@ def get_args():
 
     parser_extract = subparsers.add_parser(
         'extract',
-        help='Extract genotype info from one or more samples.')
+        help='''Extract genotype info from one or more samples.
+        The output from this step is required for the rest of the
+        fingerprinting tools. However, you do not need to run this step
+        manually since it will run automatically if the necessary files
+        are missing.''')
+    parser_extract = add_common_args(parser_extract)
 
     parser_sexmismatch = subparsers.add_parser(
-        'sexmismatch',
-        help='Check for sex mismatches.')
+        'sexmismatch', help='Check for sex mismatches.')
+    parser_sexmismatch = add_common_args(parser_sexmismatch)
+    parser_sexmismatch = add_outdir(parser_sexmismatch)
 
     parser_minor = subparsers.add_parser(
-        'minor',
-        help='Check for minor contamination.')
+        'minor', help='Check for minor contamination.')
+    parser_minor = add_common_args(parser_minor)
+    parser_minor = add_outdir(parser_minor)
 
     parser_major = subparsers.add_parser(
-        'major',
-        help='Check for major contamination.')
+        'major', help='Check for major contamination.')
+    parser_major = add_common_args(parser_major)
+    parser_major = add_outdir(parser_major)
 
     parser_genotype = subparsers.add_parser(
-        'genotype',
-        help='Genotype a set of samples.')
-
-    # project_parser = add_common_args(project_parser)
+        'genotype', help='Genotype a set of samples.')
+    parser_genotype = add_common_args(parser_genotype)
+    parser_genotype = add_outdir(parser_genotype)
 
     args = parser.parse_args()
 
-    return args
+    check_args(args)
 
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    return args
