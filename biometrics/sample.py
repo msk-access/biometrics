3ba80a69d3072cf449f57496f1ba827d93994702
murphycj2 <murphy.charlesj@icloud.com>
2020-10-20 16:22:18 -0400

    automated search for sample bam file

--- biometrics/sample.py
+++ biometrics/sample.py
@@ -1,4 +1,7 @@
+import glob
 import os
+
+from utils import exit_error
 
 
 class Sample:
@@ -6,13 +9,31 @@ class Sample:
     Class to hold information related to a single sample.
     """
 
-    def __init__(self, alignment_file=None, name=None, patient=None, sex=None, sample_type=None):
+    def __init__(self, name, alignment_file=None, patient=None, sex=None, sample_type=None, db=None):
         self.alignment_file = None
         self.name = name
         self.sex = sex
         self.patient = patient
         self.sample_type = sample_type
 
+        if db is not None:
+            self.extraction_file = os.path.join(db, self.name, '.pk')
+        else:
+            self.extraction_file = os.path.join(os.getcwd(), self.name, '.pk')
+
     def find_titlefile_alignment(self, basedir, bam_type=None):
 
         bam_basedir = os.path.join(basedir, self.patient, self.name, 'latest')
+
+        bam = glob.glob(
+            os.path.join(
+                bam_basedir,
+                '*_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX.bam'))
+
+        if not bam:
+            exit_error('Could not find BAM file for {}.'.format(self.name))
+        elif len(bam) > 1:
+            exit_error(
+                'Found more than one BAM file for {}.'.format(self.name))
+
+        self.alignment_file = bam[0]
