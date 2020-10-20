a514d808db1609769508a35265060e7f2ef0807d
murphycj2 <murphy.charlesj@icloud.com>
2020-10-20 12:24:37 -0400

    parse sample info

--- biometrics/biometrics.py
+++ biometrics/biometrics.py
@@ -1,1 +1,103 @@
-"""Main module."""
+#!/usr/bin/env python
+import sys
+
+from cli import get_args
+from sample import Sample
+import pandas as pd
+
+
+def run_extract(args):
+    pass
+
+
+def run_sexmismatch(args):
+    pass
+
+
+def run_minor_contamination(args):
+    pass
+
+
+def run_major_contamination(args):
+    pass
+
+
+def run_genotyping(args):
+    pass
+
+
+def get_samples_from_titlefile(args):
+    samples = []
+
+    for fpath in args.titlefile:
+
+        titlefile = pd.read_csv(fpath, sep='\t')
+
+        for i in titlefile.index:
+            sample = Sample(
+                patient=titlefile.at[i, 'Patient_ID'],
+                name=titlefile.at[i, 'Patient_ID'],
+                sample_type=titlefile.at[i, 'Sample_type'],
+                sex=titlefile.at[i, 'Sex'])
+
+            sample.find_titlefile_alignment(args.bam_basedir)
+            samples.append(sample)
+
+    return samples
+
+
+def get_samples_list(args):
+    samples = []
+
+    for i, bam in enumerate(args.sample_bams):
+
+        patient = args.sample_patient[i] \
+            if args.sample_patient[i] is not None else None
+        sex = args.sample_sex[i] \
+            if args.sample_sex[i] is not None else None
+        sample_type = args.sample_type[i] \
+            if args.sample_type[i] is not None else None
+        name = args.sample_name[i] \
+            if args.sample_name[i] is not None else None
+
+        sample = Sample(
+            patient=patient, name=name, sample_type=sample_type(), sex=sex)
+
+        samples.append(sample)
+
+    return samples
+
+
+def get_samples(args):
+
+    samples = []
+
+    if args.titlefile:
+        samples += get_samples_from_titlefile(args)
+
+    if args.sample_bam:
+        samples += get_samples_list(args)
+
+    return samples
+
+
+def main():
+
+    args = get_args()
+
+    samples = get_samples(args)
+
+    if args.subparser_name == 'extract':
+        run_extract(args)
+    elif args.subparser_name == 'sexmismatch':
+        run_sexmismatch(args)
+    elif args.subparser_name == 'minor':
+        run_minor_contamination(args)
+    elif args.subparser_name == 'major':
+        run_major_contamination(args)
+    elif args.subparser_name == 'genotype':
+        run_genotyping(args)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- biometrics/sample.py
+++ biometrics/sample.py
@@ -1,3 +1,4 @@
+import os
 
 
 class Sample:
@@ -5,8 +6,13 @@ class Sample:
     Class to hold information related to a single sample.
     """
 
-    def __init__(self, alignment_file, name, patient=None, sex=None):
-        self.alignment_file = alignment_file
+    def __init__(self, alignment_file=None, name=None, patient=None, sex=None, sample_type=None):
+        self.alignment_file = None
         self.name = name
         self.sex = sex
         self.patient = patient
+        self.sample_type = sample_type
+
+    def find_titlefile_alignment(self, basedir, bam_type=None):
+
+        bam_basedir = os.path.join(basedir, self.patient, self.name, 'latest')
